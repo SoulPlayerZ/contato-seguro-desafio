0b65bbc316d9c726165a26dd281008a2453439fc
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-04-05 18:21:51 -0300

    feat: Create UserPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import UserPage from "./pages/UserPage";
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Header />} />
+        <Route path="/usuario" element={<UserPage />} />
       </Routes>
     </BrowserRouter>
   )
