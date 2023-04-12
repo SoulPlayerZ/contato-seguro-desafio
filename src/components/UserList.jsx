64caeb15a6c8a8ea41a8aa591ef98bf1b99b679c
SoulPlayerZ <thyrso.kof@gmail.com>
2023-04-12 13:56:49 -0300

    feat: Implement react-icons for button icons in the info table

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,11 +1,39 @@
+import { BiEdit } from "react-icons/bi";
+import { FaEraser } from "react-icons/fa";
+
 function UserList() {
   return (
     <section>
-      <h4>Nome</h4>
-      <h4>Email</h4>
-      <h4>Telefone</h4>
-      <h4>Nascimento</h4>
-      <h4>Cidade</h4>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Telefone</th>
+            <th>Nascimento</th>
+            <th>Cidade</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Pedro</td>
+            <td>pedrim@gmail.com</td>
+            <td>21 985868598</td>
+            <td>18/02/1999</td>
+            <td>São Paulo</td>
+            <td className="add">
+              <BiEdit />
+            </td>
+            <td className="add">
+              <FaEraser />
+            </td>
+          </tr>
+          <tr>
+            <td>Carla</td>
+            <td>9.0</td>
+          </tr>
+        </tbody>
+      </table>
     </section>
   );
 }
