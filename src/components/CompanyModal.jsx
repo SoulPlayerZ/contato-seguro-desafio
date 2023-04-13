ead3e6f913d4d13192d67848e2db3e722dc05fed
Wellington_Fonseca <thyrso.kof@gmail.com>
2023-04-12 21:48:51 -0300

    feat: Implement CompanyModal component using Bootstrap

---
+++ src/components/CompanyModal.jsx
@@ -0,0 +1,47 @@
+import "../css/companyModal.css";
+
+function CompanyModal() {
+  return (
+    <div className="modal fade" id="company-modal">
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h1 className="modal-title fs-5">Inserir Empresa</h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">
+            <form>
+              <label>
+                Nome:
+                <input type="text" name="nome" maxLength="30" required />
+              </label>
+              <label>
+                CNPJ:
+                <input type="text" name="cnpj" maxLength="20" required />
+              </label>
+              <label>
+                Endereço:
+                <input type="text" name="endereco" maxLength="40" required />
+              </label>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary">
+                  Limpar
+                </button>
+                <button type="submit" className="btn btn-primary">
+                  Enviar
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CompanyModal;
